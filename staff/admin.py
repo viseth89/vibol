b9856b2d6f0d3db32146b15a5e6f9e3c3a0ada91
viseth89 <viseth89@gmail.com>
2019-01-07 11:57:50 -0600

    added few listings to test how listings will display

--- staff/admin.py
+++ staff/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from.models import Staff
 
-admin.site.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+admin.site.register(Staff, StaffAdmin)
