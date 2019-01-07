d9541c123de105d4d03872a51a78069b55224da9
viseth89 <viseth89@gmail.com>
2019-01-07 08:23:33 -0600

    listing model

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Listing(models.Model):
+    staff = models.ForeignKey(Staff, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.IntegerField()
+    qty = models.IntegerField()
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    def __str__(self):
+        return self.title
